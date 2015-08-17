5585f6febcb380c01f3a7a60a92ea3c0c19fe4fc
Angel Vanegas <angelman@gmail.com>
2015-08-17 16:56:37 -0400

    rudimentary search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,13 @@
 class SearchController < ApplicationController
   def search
+    @result = find_quip(params[:context], params[:target])
+  end
+
+  private
+
+  def find_quip(context, target)
+    unless Quip.where(context_id: context, target_id: target).empty?
+      Quip.where(context_id: context, target_id: target).sample
+    end
   end
 end
